85663685cb3a913e5db73a5b78da7ec94759f6e6
nas <nas.silver.bullet@gmail.com>
2021-02-20 15:17:53 +0900

    add

--- src/algorithm/exhaustive_search.rs
+++ src/algorithm/exhaustive_search.rs
@@ -1,3 +1,5 @@
+const MAX: i32 = 20_000_000;
+
 #[allow(dead_code)]
 fn liner_exist(a: Vec<i32>, v: i32) -> bool {
     for n in a {
@@ -22,7 +24,6 @@ fn liner_index(a: Vec<i32>, v: i32) -> i32 {
 
 #[allow(dead_code)]
 fn liner_min(a: Vec<i32>) -> i32 {
-    const MAX: i32 = 20_000_000;
     let mut min = MAX;
 
     for n in a {
@@ -69,6 +70,73 @@ fn liner_combination(a: Vec<i32>, w: i32) -> bool {
     false
 }
 
+#[allow(dead_code)]
+fn chapter_problem3_1(a: Vec<i32>, v: i32) -> i32 {
+    let mut founded_id = -1;
+    for (i, n) in a.iter().enumerate() {
+        if v == *n {
+            founded_id = i as i32;
+        }
+    }
+
+    founded_id
+}
+
+#[allow(dead_code)]
+fn chapter_problem3_2(a: Vec<i32>, v: i32) -> i32 {
+    let mut cnt = 0;
+    for n in a {
+        if v == n {
+            cnt += 1;
+        }
+    }
+
+    cnt
+}
+
+#[allow(dead_code)]
+fn chapter_problem3_3(a: Vec<i32>) -> i32 {
+    let mut min = MAX;
+    let mut second_min = MAX;
+    for n in a {
+        if min > n {
+            second_min = min;
+            min = n;
+        }
+    }
+
+    second_min
+}
+
+#[allow(dead_code)]
+fn chapter_problem3_4(a: Vec<i32>) -> i32 {
+    let mut min = MAX;
+    let mut max = 0;
+    for n in a {
+        if min > n {
+            min = n;
+        }
+
+        if n > max {
+            max = n;
+        }
+    }
+
+    max - min
+}
+
+#[allow(dead_code)]
+fn chapter_problem3_5(mut a: Vec<i32>) -> i32 {
+    let mut cnt = 0;
+
+    while a.iter().all(|n| n % 2 == 0) {
+        a = a.iter().map(|n| n / 2).collect();
+        cnt += 1;
+    }
+
+    cnt
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -100,5 +168,36 @@ mod tests {
     fn test_liner_combination() {
         assert_eq!(liner_combination(vec![1, 2, 4, 5, 11], 10), true);
         assert_eq!(liner_combination(vec![1, 5, 8, 11], 10), false);
+    }
+
+    #[test]
+    fn test_chapter_problem3_1() {
+        assert_eq!(chapter_problem3_1(vec![4, 3, 12, 7, 11, 7], 7), 5);
+        assert_eq!(chapter_problem3_1(vec![4, 3, 12, 7, 11], 8), -1);
+    }
+
+    #[test]
+    fn test_chapter_problem3_2() {
+        assert_eq!(chapter_problem3_2(vec![4, 3, 12, 11], 7), 0);
+        assert_eq!(chapter_problem3_2(vec![4, 3, 12, 7, 11], 7), 1);
+        assert_eq!(chapter_problem3_2(vec![4, 3, 12, 7, 11, 7], 7), 2);
+    }
+
+    #[test]
+    fn test_chapter_problem3_3() {
+        assert_eq!(chapter_problem3_3(vec![4, 3, 12, 11]), 4);
+        assert_eq!(chapter_problem3_3(vec![4, 3, 12, 11, 2]), 3);
+    }
+
+    #[test]
+    fn test_chapter_problem3_4() {
+        assert_eq!(chapter_problem3_4(vec![4, 3, 12, 11]), 9);
+        assert_eq!(chapter_problem3_4(vec![4, 3, 12, 11, 2]), 10);
+    }
+
+    #[test]
+    fn test_chapter_problem3_5() {
+        assert_eq!(chapter_problem3_5(vec![8, 12, 24, 32]), 2);
+        assert_eq!(chapter_problem3_5(vec![4, 3, 12, 11, 2]), 0);
     }
 }
